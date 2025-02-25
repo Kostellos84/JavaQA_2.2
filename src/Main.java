996655eaaaeed3dc3c29c69b3762510ce0454a50
Konstantin S. <kostellos84@mail.ru>
2025-02-25 17:08:25 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int supplement = 1000;
+        int bonuses = (supplement / 100);
+        if (supplement >= 1000) {
+            System.out.println("На вашем счету " + (supplement / 100 + score + supplement) + " рублей");
+            System.out.println("Сумма начисленых бонусов: " + bonuses + " рублей");
+        } else {
+            System.out.println("На вашем счету " + (supplement + score) + " Рублей");
+            System.out.println("Для получения бонусов сумма пополнения доложна быть более тысячи рублей");
+        }
+    }
+
+}
